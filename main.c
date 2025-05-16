19deeba02fd3620f725098d8d4b50cedabf07322
ZaCc42 <zalac.mario1@gmail.com>
2025-05-16 14:22:04 +0200

    dice roll works

--- main.c
+++ main.c
@@ -8,6 +8,13 @@ int main()
 
    
     // Your code goes here
+    int dice_rolls[100];
+    for(int i=0; i<100; i++){
+        dice_rolls[i] = (rand() %6)+1;
+        printf("rool %d\n", dice_rolls[i]);
+    }
+
+
 
     return 0;
 }
